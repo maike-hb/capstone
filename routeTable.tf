2f93f5ecdd79c13389770a3468e31d54bdeec578
Maike HB <maike.hermann@gmx.net>
2025-04-15 17:08:23 +0200

    add route_table

---
+++ routeTable.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "wordpress_route_table" {
+  vpc_id = aws_vpc.wordpress_vpc.id
+
+  #route {
+  #  cidr_block = "10.0.1.0/24"
+  #  gateway_id = aws_internet_gateway.igateway.id
+  #}
+
+  #route {
+  #  ipv6_cidr_block        = "::/0"
+  #  egress_only_gateway_id = aws_egress_only_internet_gateway.igateway.id
+  #}
+
+  tags = {
+    Name = "wordpress_route_table"
+  }
+}
