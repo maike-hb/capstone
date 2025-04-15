a54d47731df8ed9cd3a33a6b66f3774627c93f16
Maike HB <maike.hermann@gmx.net>
2025-04-15 17:27:12 +0200

    add subnet association

--- routeTable.tf
+++ routeTable.tf
@@ -1,6 +1,6 @@
 resource "aws_route_table" "wordpress_route_table" {
   vpc_id = aws_vpc.wordpress_vpc.id
-
+  
   #route {
   #  cidr_block = "10.0.1.0/24"
   #  gateway_id = aws_internet_gateway.igateway.id
@@ -14,6 +14,11 @@ resource "aws_route_table" "wordpress_route_table" {
   tags = {
     Name = "wordpress_route_table"
   }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.wordpress_subnet.id
+  route_table_id = aws_route_table.wordpress_route_table.id
 }
 
 
