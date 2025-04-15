19f477e03b401f47ca3ef08f0daa1d30c162f8cf
Maike HB <maike.hermann@gmx.net>
2025-04-15 17:30:27 +0200

    fixed subnet name

--- routeTable.tf
+++ routeTable.tf
@@ -1,5 +1,6 @@
 resource "aws_route_table" "wordpress_route_table" {
   vpc_id = aws_vpc.wordpress_vpc.id
+
   
   #route {
   #  cidr_block = "10.0.1.0/24"
@@ -17,7 +18,7 @@ resource "aws_route_table" "wordpress_route_table" {
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.wordpress_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.wordpress_route_table.id
 }
 
