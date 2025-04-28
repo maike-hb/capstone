4bb2870c7b02d0fe834026d777667abe46ab8c65
Maike HB <maike.hermann@gmx.net>
2025-04-28 09:56:21 +0200

    map public id

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.wordpress_vpc.id
   cidr_block = "10.0.1.0/24"
 
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet"
   }
